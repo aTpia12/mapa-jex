eb16b61e7394268b55352c1f6e25f84b3e1c2ad9
Augusto Tapia <atdevlop@gmail.com>
2023-04-18 22:20:41 -0600

    Mapa JEX Xalapa 2023

---
+++ index.php
@@ -0,0 +1,181 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="MobileOptimized" content="320" />
+    <title>Mapa JEX</title>
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css" integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
+    <script src="https://code.jquery.com/jquery-3.4.1.slim.min.js" integrity="sha384-J6qa4849blE2+poT4WnyKhv5vZF5SrPo0iEjwBvKU7imGFAV0wwj1yYfoRSJoZ+n" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js" integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo" crossorigin="anonymous"></script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.min.js" integrity="sha384-wfSDF2E50Y2D1uUdj0O3uMBJnjuUD4Ih7YwaYd1iqfktj0Uod8GCExl3Og8ifwB6" crossorigin="anonymous"></script>
+</head>
+
+<body>
+    <audio src="mapa.mp3" autoplay loop></audio>
+
+    <map name="image-map-1">
+        <area style="cursor: pointer" data-toggle="modal" data-target="#exampleModal" onclick="im(1)" shape="circle" coords="379,565,15">
+        <area style="cursor: pointer" data-toggle="modal" data-target="#exampleModal" onclick="im(2)" shape="circle" coords="470,508,15">
+        <area style="cursor: pointer" data-toggle="modal" data-target="#exampleModal" onclick="im(3)" shape="circle" coords="470,480,15">
+        <area style="cursor: pointer" data-toggle="modal" data-target="#exampleModal" onclick="im(4)" shape="circle" coords="470,460,15">
+        <area style="cursor: pointer" data-toggle="modal" data-target="#exampleModal" onclick="im(5)" shape="circle" coords="510,503,15">
+        <area style="cursor: pointer" data-toggle="modal" data-target="#exampleModal" onclick="im(6)" shape="circle" coords="535,503,15">
+        <area style="cursor: pointer" data-toggle="modal" data-target="#exampleModal" onclick="im(7)" shape="circle" coords="604,487,15">
+        <area style="cursor: pointer" data-toggle="modal" data-target="#exampleModal" onclick="im(8)" shape="circle" coords="635,460,15">
+        <area style="cursor: pointer" data-toggle="modal" data-target="#exampleModal" onclick="im(9)" shape="circle" coords="604,450,15">
+        <area style="cursor: pointer" data-toggle="modal" data-target="#exampleModal" onclick="im(10)" shape="circle" coords="625,405,15">
+        <area style="cursor: pointer" data-toggle="modal" data-target="#exampleModal" onclick="im(11)" shape="circle" coords="580,425,15">
+        <area style="cursor: pointer" data-toggle="modal" data-target="#exampleModal" onclick="im(12)" shape="circle" coords="545,390,15">
+        <area style="cursor: pointer" data-toggle="modal" data-target="#exampleModal" onclick="im(13)" shape="circle" coords="490,355,15">
+        <area style="cursor: pointer" data-toggle="modal" data-target="#exampleModal" onclick="im(14)" shape="circle" coords="455,350,15">
+        <area style="cursor: pointer" data-toggle="modal" data-target="#exampleModal" onclick="im(15)" shape="circle" coords="545,305,15">
+        <area style="cursor: pointer" data-toggle="modal" data-target="#exampleModal" onclick="im(16)" shape="circle" coords="565,325,15">
+        <area style="cursor: pointer" data-toggle="modal" data-target="#exampleModal" onclick="im(17)" shape="circle" coords="605,325,15">
+        <area style="cursor: pointer" data-toggle="modal" data-target="#exampleModal" onclick="im(18)" shape="circle" coords="500,230,15">
+        <area style="cursor: pointer" data-toggle="modal" data-target="#exampleModal" onclick="im(19)" shape="circle" coords="585,230,15">
+        <area style="cursor: pointer" data-toggle="modal" data-target="#exampleModal" onclick="im(20)" shape="circle" coords="655,285,15">
+        <area style="cursor: pointer" data-toggle="modal" data-target="#exampleModal" onclick="im(21)" shape="circle" coords="655,321,15">
+        <area style="cursor: pointer" data-toggle="modal" data-target="#exampleModal" onclick="im(22)" shape="circle" coords="645,365,15">
+        <area style="cursor: pointer" data-toggle="modal" data-target="#exampleModal" onclick="im(23)" shape="circle" coords="701,295,15">
+        <area style="cursor: pointer" data-toggle="modal" data-target="#exampleModal" onclick="im(24)" shape="circle" coords="711,210,15">
+        <area style="cursor: pointer" data-toggle="modal" data-target="#exampleModal" onclick="im(25)" shape="circle" coords="595,185,15">
+        <area style="cursor: pointer" data-toggle="modal" data-target="#exampleModal" onclick="im(26)" shape="circle" coords="560,155,15">
+        <area style="cursor: pointer" data-toggle="modal" data-target="#exampleModal" onclick="im(27)" shape="circle" coords="615,145,15">
+        <area style="cursor: pointer" data-toggle="modal" data-target="#exampleModal" onclick="im(28)" shape="circle" coords="685,165,15">
+        <area style="cursor: pointer" data-toggle="modal" data-target="#exampleModal" onclick="im(29)" shape="circle" coords="805,145,15">
+        <area style="cursor: pointer" data-toggle="modal" data-target="#exampleModal" onclick="im(30)" shape="circle" coords="775,235,15">
+        <area style="cursor: pointer" data-toggle="modal" data-target="#exampleModal" onclick="im(31)" shape="circle" coords="835,215,15">
+        <area style="cursor: pointer" data-toggle="modal" data-target="#exampleModal" onclick="im(32)" shape="circle" coords="865,225,15">
+        <area style="cursor: pointer" data-toggle="modal" data-target="#exampleModal" onclick="im(33)" shape="circle" coords="975,145,15">
+        <area style="cursor: pointer" data-toggle="modal" data-target="#exampleModal" onclick="im(34)" shape="circle" coords="1113,240,15">
+        <area style="cursor: pointer" data-toggle="modal" data-target="#exampleModal" onclick="im(35)" shape="circle" coords="1313,300,15">
+        <area style="cursor: pointer" data-toggle="modal" data-target="#exampleModal" onclick="im(36)" shape="circle" coords="1263,320,15">
+        <area style="cursor: pointer" data-toggle="modal" data-target="#exampleModal" onclick="im(37)" shape="circle" coords="1333,320,15">
+        <area style="cursor: pointer" data-toggle="modal" data-target="#exampleModal" onclick="im(38)" shape="circle" coords="1163,380,15">
+        <area style="cursor: pointer" data-toggle="modal" data-target="#exampleModal" onclick="im(39)" shape="circle" coords="1183,320,15">
+        <area style="cursor: pointer" data-toggle="modal" data-target="#exampleModal" onclick="im(40)" shape="circle" coords="1163,300,15">
+        <area style="cursor: pointer" data-toggle="modal" data-target="#exampleModal" onclick="im(41)" shape="circle" coords="1143,320,15">
+        <area style="cursor: pointer" data-toggle="modal" data-target="#exampleModal" onclick="im(42)" shape="circle" coords="1073,260,15">
+        <area style="cursor: pointer" data-toggle="modal" data-target="#exampleModal" onclick="im(43)" shape="circle" coords="1033,390,15">
+        <area style="cursor: pointer" data-toggle="modal" data-target="#exampleModal" onclick="im(44)" shape="circle" coords="1017,300,15">
+        <area style="cursor: pointer" data-toggle="modal" data-target="#exampleModal" onclick="im(45)" shape="circle" coords="977,340,15">
+        <area style="cursor: pointer" data-toggle="modal" data-target="#exampleModal" onclick="im(46)" shape="circle" coords="953,420,15">
+        <area style="cursor: pointer" data-toggle="modal" data-target="#exampleModal" onclick="im(47)" shape="circle" coords="933,425,15">
+        <area style="cursor: pointer" data-toggle="modal" data-target="#exampleModal" onclick="im(48)" shape="circle" coords="938,405,15">
+        <area style="cursor: pointer" data-toggle="modal" data-target="#exampleModal" onclick="im(49)" shape="circle" coords="918,355,15">
+        <area style="cursor: pointer" data-toggle="modal" data-target="#exampleModal" onclick="im(50)" shape="circle" coords="878,325,15">
+        <area style="cursor: pointer" data-toggle="modal" data-target="#exampleModal" onclick="im(51)" shape="circle" coords="878,385,15">
+        <area style="cursor: pointer" data-toggle="modal" data-target="#exampleModal" onclick="im(52)" shape="circle" coords="858,350,15">
+        <area style="cursor: pointer" data-toggle="modal" data-target="#exampleModal" onclick="im(53)" shape="circle" coords="844,290,15">
+        <area style="cursor: pointer" data-toggle="modal" data-target="#exampleModal" onclick="im(54)" shape="circle" coords="824,305,15">
+        <area style="cursor: pointer" data-toggle="modal" data-target="#exampleModal" onclick="im(55)" shape="circle" coords="824,395,15">
+        <area style="cursor: pointer" data-toggle="modal" data-target="#exampleModal" onclick="im(56)" shape="circle" coords="774,345,15">
+        <area style="cursor: pointer" data-toggle="modal" data-target="#exampleModal" onclick="im(57)" shape="circle" coords="774,385,15">
+        <area style="cursor: pointer" data-toggle="modal" data-target="#exampleModal" onclick="im(58)" shape="circle" coords="780,420,15">
+        <area style="cursor: pointer" data-toggle="modal" data-target="#exampleModal" onclick="im(59)" shape="circle" coords="740,420,15">
+        <area style="cursor: pointer" data-toggle="modal" data-target="#exampleModal" onclick="im(60)" shape="circle" coords="705,410,15">
+        <area style="cursor: pointer" data-toggle="modal" data-target="#exampleModal" onclick="im(61)" shape="circle" coords="810,450,15">
+        <area style="cursor: pointer" data-toggle="modal" data-target="#exampleModal" onclick="im(62)" shape="circle" coords="834,415,15">
+        <area style="cursor: pointer" data-toggle="modal" data-target="#exampleModal" onclick="im(63)" shape="circle" coords="843,455,15">
+        <area style="cursor: pointer" data-toggle="modal" data-target="#exampleModal" onclick="im(64)" shape="circle" coords="863,485,15">
+        <area style="cursor: pointer" data-toggle="modal" data-target="#exampleModal" onclick="im(65)" shape="circle" coords="883,455,15">
+        <area style="cursor: pointer" data-toggle="modal" data-target="#exampleModal" onclick="im(66)" shape="circle" coords="893,425,15">
+        <area style="cursor: pointer" data-toggle="modal" data-target="#exampleModal" onclick="im(67)" shape="circle" coords="1123,430,15">
+        <area style="cursor: pointer" data-toggle="modal" data-target="#exampleModal" onclick="im(68)" shape="circle" coords="1205,460,15">
+        <area style="cursor: pointer" data-toggle="modal" data-target="#exampleModal" onclick="im(69)" shape="circle" coords="1195,585,15">
+        <area style="cursor: pointer" data-toggle="modal" data-target="#exampleModal" onclick="im(70)" shape="circle" coords="1155,595,15">
+        <area style="cursor: pointer" data-toggle="modal" data-target="#exampleModal" onclick="im(71)" shape="circle" coords="1095,547,15">
+        <area style="cursor: pointer" data-toggle="modal" data-target="#exampleModal" onclick="im(72)" shape="circle" coords="1087,587,15">
+        <area style="cursor: pointer" data-toggle="modal" data-target="#exampleModal" onclick="im(73)" shape="circle" coords="1067,577,15">
+        <area style="cursor: pointer" data-toggle="modal" data-target="#exampleModal" onclick="im(74)" shape="circle" coords="1033,577,15">
+        <area style="cursor: pointer" data-toggle="modal" data-target="#exampleModal" onclick="im(75)" shape="circle" coords="1042,527,15">
+        <area style="cursor: pointer" data-toggle="modal" data-target="#exampleModal" onclick="im(76)" shape="circle" coords="873,617,15">
+        <area style="cursor: pointer" data-toggle="modal" data-target="#exampleModal" onclick="im(77)" shape="circle" coords="843,597,15">
+        <area style="cursor: pointer" data-toggle="modal" data-target="#exampleModal" onclick="im(78)" shape="circle" coords="723,577,15">
+        <area style="cursor: pointer" data-toggle="modal" data-target="#exampleModal" onclick="im(79)" shape="circle" coords="683,577,15">
+    </map>
+    <center><img src="img/mapa.png" usemap="#image-map-1" alt="Mapa de imagen de ejemplo"></center>
+
+
+    <!-- Modal -->
+    <div class="modal fade exampleModal-modal-lg" id="exampleModal" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
+        <div class="modal-dialog modal-lg" role="document">
+            <div class="modal-content">
+                <div class="modal-header">
+                    <h5 class="modal-title" id="exampleModalLabel"></h5>
+                    <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                        <span aria-hidden="true">&times;</span>
+                    </button>
+                </div>
+                <div class="modal-body">
+                    <div class="container" id="igApp">
+
+                    </div>
+                </div>
+                <div class="modal-footer">
+                    <button type="button" class="btn btn-secondary" data-dismiss="modal">Cerrar</button>
+
+                </div>
+            </div>
+        </div>
+    </div>
+    <script src="js/app.js"></script>
+    <!-- <script>
+        //<![CDATA[
+        // Fall v2.0 By MaxxBlade - http://www.maxxblade.co.uk/fall
+        var fallObjects = new Array();
+
+        function newObject(url, height, width) {
+            fallObjects[fallObjects.length] = new Array(url, height, width);
+        }
+        var numObjs = 15,
+            waft = 100,
+            fallSpeed = 5,
+            wind = -1;
+
+        newObject("img/hoja_1.png", 29, 15);
+        //newObject("hoja_2.png", 27, 23);
+
+        function winSize() {
+            winWidth = (moz) ? window.innerWidth : document.body.clientWidth;
+            winHeight = (moz) ? window.innerHeight : document.body.clientHeight;
+        }
+
+        function fallObject(num, vari, nu) {
+            objects[num] = new Array(parseInt(Math.random() * (winWidth - waft)), -30, (parseInt(Math.random() * waft)) * ((Math.random() > 0.5) ? 1 : -1), 0.02 + Math.random() / 20, 0, 1 + parseInt(Math.random() * fallSpeed), vari, fallObjects[vari][1], fallObjects[vari][2]);
+            if (nu == 1) {
+                document.write('<img id="fO' + i + '" style="position:absolute;z-index:-1;background:none;border:0;padding:0;box-shadow:none;" src="' + fallObjects[vari][0] + '">');
+            }
+        }
+
+        function fall() {
+            for (i = 0; i < numObjs; i++) {
+                var fallingObject = document.getElementById('fO' + i);
+                if ((objects[i][1] > (winHeight - (objects[i][5] + objects[i][7]))) || (objects[i][0] > (winWidth - (objects[i][2] + objects[i][8])))) {
+                    fallObject(i, objects[i][6], 0);
+                }
+                objects[i][0] += wind;
+                objects[i][1] += objects[i][5];
+                objects[i][4] += objects[i][3];
+                with(fallingObject.style) {
+                    top = objects[i][1] + winOffset + "px";
+                    left = objects[i][0] + (objects[i][2] * Math.cos(objects[i][4])) + "px";
+                }
+            }
+            setTimeout("fall()", 31);
+        }
+        var objects = new Array(),
+            winOffset = 0,
+            winHeight, winWidth, togvis, moz = (document.getElementById && !document.all) ? 1 : 0;
+        winSize();
+        for (i = 0; i < numObjs; i++) {
+            fallObject(i, parseInt(Math.random() * fallObjects.length), 1);
+        }
+        window.onresize = winSize;
+        fall();
+        //]]>
+    </script>-->
+</body>
+
+</html>
---
+++ js/app.js
@@ -0,0 +1,8 @@
+
+
+function im(id){
+
+    //console.log(id);
+    document.getElementById("igApp").innerHTML = `<img class="img-fluid" src="img/piezas/${id}.jpg">`;
+
+}
